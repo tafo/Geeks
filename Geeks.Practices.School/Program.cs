2628c12d33f583042dacb43d268718171d255fd7
Tayfun Yirdem <tayfunyirdem@gmail.com>
2020-02-26 12:01:58 +0300

    The largest element in the array

--- Geeks.Practices.School/Program.cs
+++ Geeks.Practices.School/Program.cs
@@ -5,9 +5,28 @@
     /// </summary>
     internal class Program
     {
-        private static void Main()
+        private static void Main(string[] args)
         {
-            SumOfArrayElements.Run();
+            int index;
+            switch (args)
+            {
+                case null:
+                    index = 2;
+                    break;
+                default:
+                    int.TryParse(args[0], out index);
+                    break;
+            }
+
+            switch (index)
+            {
+                case 0:
+                    SumOfArrayElements.Run();
+                    break;
+                case 1:
+                    LargestElementInArray.Run();
+                    break;
+            }
         }
     }
 }
