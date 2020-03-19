3ac12a49ebaf5037649019f797e1dc60a7d49014
Tayfun Yirdem <tayfunyirdem@gmail.com>
2020-03-19 23:09:23 +0300

    Arrays - Basic - CountAvg - Add - Linq solution

--- Geeks.Practices/Arrays/Basic/CountAvg.cs
+++ Geeks.Practices/Arrays/Basic/CountAvg.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using Geeks.Practices.Helper;
 
 namespace Geeks.Practices.Arrays.Basic
@@ -55,6 +56,31 @@ namespace Geeks.Practices.Arrays.Basic
     [SuppressMessage("ReSharper", "PossibleNullReferenceException")]
     public class CountAvg
     {
+        /// <summary>
+        /// The execution time is 0.14
+        /// </summary>
+        public static void RunLinq()
+        {
+            var testCount = int.Parse(Console.ReadLine());
+            var tests = new string[testCount][];
+
+            for (var i = 0; i < testCount; i++)
+            {
+                tests[i] = new string[2];
+                tests[i][0] = Console.ReadLine();
+                tests[i][1] = Console.ReadLine().TrimEnd();
+            }
+
+            foreach (var test in tests)
+            {
+                var split = test[0].Split(' ');
+                var n = int.Parse(split[0]);
+                var x = int.Parse(split[1]);
+                var numbers = StringScanner.GetPositiveInt(test[1], n);
+                Console.WriteLine(string.Join(' ', numbers.Select(k => numbers.Count(y => y == (k + x) / 2))));
+            }
+        }
+
         /// <summary>
         /// The execution time is 0.15
         /// </summary>
