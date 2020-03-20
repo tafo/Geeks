3a6f655e91d240987554394949ce90658ba9af21
Tayfun Yirdem <tayfunyirdem@gmail.com>
2020-03-20 10:51:58 +0300

    Arrays - Basic - Distinct - Add - CompareToTest solution

--- Geeks.Practices/Arrays/Basic/Distinct.cs
+++ Geeks.Practices/Arrays/Basic/Distinct.cs
@@ -39,6 +39,90 @@ namespace Geeks.Practices.Arrays.Basic
     public class Distinct
     {
         /// <summary>
+        /// The execution time is 0.13
+        /// </summary>
+        public static void RunCompareToTest()
+        {
+            var testCount = int.Parse(Console.ReadLine());
+            var tests = new string[testCount][];
+
+            for (var i = 0; i < testCount; i++)
+            {
+                tests[i] = new string[2];
+                tests[i][0] = Console.ReadLine();
+                tests[i][1] = Console.ReadLine().TrimEnd();
+            }
+
+            foreach (var test in tests)
+            {
+                var n = int.Parse(test[0]);
+                var numbers = StringScanner.GetPositiveInt(test[1], n);
+                var resultBuilder = new StringBuilder(n);
+                resultBuilder.AppendFormat("{0} ", numbers[0]);
+                for (var i = 1; i < n; i++)
+                {
+                    var exists = false;
+                    for (var k = 0; k < i; k++)
+                    {
+                        if (numbers[i] == numbers[k])
+                        {
+                            exists = true;
+                        }
+                    }
+
+                    if (!exists)
+                    {
+                        resultBuilder.AppendFormat("{0} ", numbers[i]);
+                    }
+                }
+
+                Console.WriteLine(resultBuilder.ToString());
+            }
+        }
+
+        /// <summary>
+        /// The execution time is 0.13
+        /// </summary>
+        public static void RunTest()
+        {
+            var testCount = int.Parse(Console.ReadLine());
+            var tests = new string[testCount][];
+
+            for (var i = 0; i < testCount; i++)
+            {
+                tests[i] = new string[2];
+                tests[i][0] = Console.ReadLine();
+                tests[i][1] = Console.ReadLine().TrimEnd();
+            }
+
+            foreach (var test in tests)
+            {
+                var n = int.Parse(test[0]);
+                var numbers = StringScanner.GetPositiveInt(test[1], n);
+                var resultBuilder = new StringBuilder(n);
+                resultBuilder.Append(numbers[0] + " ");
+                for (var i = 1; i < n; i++)
+                {
+                    var exists = false;
+                    for (var k = 0; k < i; k++)
+                    {
+                        if (numbers[i] == numbers[k])
+                        {
+                            exists = true;
+                        }
+                    }
+
+                    if (!exists)
+                    {
+                        resultBuilder.Append(numbers[i] + " ");
+                    }
+                }
+
+                Console.WriteLine(resultBuilder.ToString());
+            }
+        }
+
+        /// <summary>
         /// The execution time is 0.12
         /// </summary>
         public static void RunCompareTo()
@@ -59,6 +143,7 @@ namespace Geeks.Practices.Arrays.Basic
                 var numbers = StringScanner.GetPositiveInt(test[1], n);
                 var resultBuilder = new StringBuilder(n);
                 resultBuilder.Append(numbers[0]);
+                resultBuilder.Append(" ");
                 for (var i = 1; i < n; i++)
                 {
                     var exists = false;
@@ -76,6 +161,7 @@ namespace Geeks.Practices.Arrays.Basic
                         resultBuilder.Append(" ");
                     }
                 }
+
                 Console.WriteLine(resultBuilder.ToString());
             }
         }
@@ -116,6 +202,7 @@ namespace Geeks.Practices.Arrays.Basic
                         numbers[counter++] = numbers[i];
                     }
                 }
+
                 var result = new int[counter];
                 Array.Copy(numbers, 0, result, 0, counter);
                 Console.WriteLine(new StringBuilder(counter).AppendJoin(' ', result).ToString());
