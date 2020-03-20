97847752b43df881dd9ee9eb123a71875c98db07
Tayfun Yirdem <tayfunyirdem@gmail.com>
2020-03-20 09:10:32 +0300

    Arrays - Basic - DescendingDigits - Add - SingleLineLinq solution

--- Geeks.Practices/Arrays/Basic/DescendingDigits.cs
+++ Geeks.Practices/Arrays/Basic/DescendingDigits.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using Geeks.Practices.Helper;
 
 namespace Geeks.Practices.Arrays.Basic
@@ -47,6 +48,27 @@ namespace Geeks.Practices.Arrays.Basic
     [SuppressMessage("ReSharper", "PossibleNullReferenceException")]
     public class DescendingDigits
     {
+        /// <summary>
+        /// The execution time is 3.42!
+        /// * Interesting
+        /// </summary>
+        public static void RunSingleLineLinq()
+        {
+            var testCount = int.Parse(Console.ReadLine());
+            var tests = new string[testCount];
+
+            for (var i = 0; i < testCount; i++)
+            {
+                Console.ReadLine();
+                tests[i] = Console.ReadLine().TrimEnd();
+            }
+
+            foreach (var test in tests)
+            {
+                Console.WriteLine(string.Join(string.Empty, test.Split(' ').Select(int.Parse).OrderByDescending(x => x)));
+            }
+        }
+
         /// <summary>
         /// The execution time is 1.37
         /// </summary>
