670e9ec31e17f21733fa69a36fd97ee7c69952a5
Tayfun Yirdem <tayfunyirdem@gmail.com>
2020-03-21 11:29:05 +0300

    Arrays - Basic - AlternateSorting - Add - Solution - Linq

--- Geeks.Practices/Arrays/Basic/AlternateSorting.cs
+++ Geeks.Practices/Arrays/Basic/AlternateSorting.cs
@@ -54,6 +54,26 @@ namespace Geeks.Practices.Arrays.Basic
         /// <summary>
         /// The execution time is 0.10
         /// </summary>
+        public static void RunLinq()
+        {
+            var testCount = int.Parse(Console.ReadLine());
+            var tests = new int[testCount][];
+
+            for (var i = 0; i < testCount; i++)
+            {
+                Console.ReadLine();
+                tests[i] = Console.ReadLine().TrimEnd().Split(' ').Select(int.Parse).OrderBy(x => x).ToArray();
+            }
+
+            foreach (var test in tests)
+            {
+                Console.WriteLine(string.Join(' ', test.Skip(test.Length / 2).Reverse().Interleave(test.Take(test.Length / 2))));
+            }
+        }
+
+        /// <summary>
+        /// The execution time is 0.10
+        /// </summary>
         public static void RunMix()
         {
             var testCount = int.Parse(Console.ReadLine());
