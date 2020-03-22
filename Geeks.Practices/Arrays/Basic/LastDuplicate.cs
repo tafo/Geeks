a72d85deb3fed97b2d98bb99e25024bcd410ddb5
Tayfun Yirdem <tayfunyirdem@gmail.com>
2020-03-22 11:45:11 +0300

    Arrays - Basic - LastDuplicate - Add - Solution - CompareToLinq

--- Geeks.Practices/Arrays/Basic/LastDuplicate.cs
+++ Geeks.Practices/Arrays/Basic/LastDuplicate.cs
@@ -59,6 +59,27 @@ namespace Geeks.Practices.Arrays.Basic
         /// <summary>
         /// The execution time is 0.09
         /// </summary>
+        public static void RunCompareToLinq()
+        {
+            var testCount = int.Parse(Console.ReadLine());
+            var tests = new string[testCount][];
+
+            for (var i = 0; i < testCount; i++)
+            {
+                Console.ReadLine();
+                tests[i] = Console.ReadLine().TrimEnd().Split(' ');
+            }
+
+            foreach (var test in tests)
+            {
+                var lastDuplicate = test.GroupBy(x => x).LastOrDefault(x => x.Count() > 1);
+                Console.WriteLine(lastDuplicate == null ? "-1" : $"{Array.LastIndexOf(test, lastDuplicate.Key)} {lastDuplicate.Key}");
+            }
+        }
+
+        /// <summary>
+        /// The execution time is 0.09
+        /// </summary>
         public static void RunLinq()
         {
             var testCount = int.Parse(Console.ReadLine());
