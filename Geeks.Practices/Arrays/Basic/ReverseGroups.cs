871b9885ee85e05e5600cada90297f21ae9f10c7
Tayfun Yirdem <tayfunyirdem@gmail.com>
2020-03-22 17:00:51 +0300

    Arrays - Basic - ReverseGroups - Add - Solution - Linq

--- Geeks.Practices/Arrays/Basic/ReverseGroups.cs
+++ Geeks.Practices/Arrays/Basic/ReverseGroups.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using Geeks.Practices.Helper;
 
 namespace Geeks.Practices.Arrays.Basic
@@ -71,6 +72,32 @@ namespace Geeks.Practices.Arrays.Basic
     [SuppressMessage("ReSharper", "PossibleNullReferenceException")]
     public class ReverseGroups
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        public static void RunLinq()
+        {
+            var testCount = int.Parse(Console.ReadLine());
+            var tests = new string[testCount][];
+
+            for (var i = 0; i < testCount; i++)
+            {
+                tests[i] = new string[2];
+                tests[i][0] = Console.ReadLine();
+                tests[i][1] = Console.ReadLine().TrimEnd();
+            }
+
+            foreach (var test in tests)
+            {
+                var split = test[0].Split(' ');
+                var n = double.Parse(split[0]);
+                var k = int.Parse(split[1]);
+                var elements = test[1].Split(' ');
+                Console.WriteLine(string.Join(' ',
+                    Enumerable.Range(0, (int) Math.Ceiling(n / k)).SelectMany(x => elements.Skip(x * k).Take(k).Reverse())));
+            }
+        }
+
         /// <summary>
         /// The execution time of the equivalent JAVA solution is 1.31. But, ... Anyway!
         /// </summary>
