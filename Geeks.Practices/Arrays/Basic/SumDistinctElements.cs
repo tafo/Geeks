0f84fdbdc474960103f18ee5a406b074113c264a
Tayfun Yirdem <tayfunyirdem@gmail.com>
2020-03-26 14:05:58 +0300

    Arrays - Basic - SumDistinctElements - Add- Solution - Mix

--- Geeks.Practices/Arrays/Basic/SumDistinctElements.cs
+++ Geeks.Practices/Arrays/Basic/SumDistinctElements.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using System.Runtime.InteropServices.ComTypes;
 using Geeks.Practices.Helper;
 
@@ -41,6 +42,30 @@ namespace Geeks.Practices.Arrays.Basic
     [SuppressMessage("ReSharper", "PossibleNullReferenceException")]
     public class SumDistinctElements
     {
+        /// <summary>
+        /// The execution time is 0.09
+        /// * Better!
+        /// </summary>
+        public static void RunMix()
+        {
+            var testCount = int.Parse(Console.ReadLine());
+            var tests = new string[testCount][];
+
+            for (var i = 0; i < testCount; i++)
+            {
+                tests[i] = new string[2];
+                tests[i][0] = Console.ReadLine();
+                tests[i][1] = Console.ReadLine().TrimEnd();
+            }
+
+            foreach (var test in tests)
+            {
+                var n = int.Parse(test[0]);
+                var numbers = StringScanner.GetPositiveInt(test[1], n);
+                Console.WriteLine(numbers.Distinct().Sum());
+            }
+        }
+
         /// <summary>
         /// The execution time is 0.13
         /// * !!!
