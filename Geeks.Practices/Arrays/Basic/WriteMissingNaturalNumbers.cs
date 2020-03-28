1580752fbb18d3113d0cbd04b05cc6b8775fc8a9
Tayfun Yirdem <tayfunyirdem@gmail.com>
2020-03-28 19:56:36 +0300

    Arrays - Basic - WriteMissingNaturalNumbers - Add - Solution - Mix

--- Geeks.Practices/Arrays/Basic/WriteMissingNaturalNumbers.cs
+++ Geeks.Practices/Arrays/Basic/WriteMissingNaturalNumbers.cs
@@ -46,6 +46,33 @@ namespace Geeks.Practices.Arrays.Basic
     public class WriteMissingNaturalNumbers
     {
         /// <summary>
+        /// The execution time is 0.15
+        /// </summary>
+        public static void RunMix()
+        {
+            var testCount = int.Parse(Console.ReadLine());
+            var tests = new string[testCount][];
+
+            for (var i = 0; i < testCount; i++)
+            {
+                tests[i] = new string[2];
+                tests[i][0] = Console.ReadLine();
+                tests[i][1] = Console.ReadLine().TrimEnd();
+            }
+
+            // 3 5 9
+            foreach (var test in tests)
+            {
+                var split = test[0].Split(' ');
+                var n = int.Parse(split[0]);
+                var k = int.Parse(split[1]);
+                var numbers = StringScanner.GetInt(test[1], n);
+                Array.Sort(numbers);
+                Console.WriteLine(string.Join(' ', Enumerable.Range(1, 2 * n).Except(numbers).Take(k)));
+            }
+        }
+
+        /// <summary>
         /// The execution time is 0.11
         /// </summary>
         public static void RunLoop()
