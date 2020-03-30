18526dda6ad4622efe75f106b9cbfe48e47f17bf
Tayfun Yirdem <tayfunyirdem@gmail.com>
2020-03-30 16:31:14 +0300

    Arrays - Basic - CountSameNumbers - Add - Solution - Mix

--- Geeks.Practices/Arrays/Basic/CountSameNumbers.cs
+++ Geeks.Practices/Arrays/Basic/CountSameNumbers.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using Geeks.Practices.Helper;
 
 namespace Geeks.Practices.Arrays.Basic
@@ -39,12 +40,35 @@ namespace Geeks.Practices.Arrays.Basic
     [SuppressMessage("ReSharper", "InvalidXmlDocComment")]
     [SuppressMessage("ReSharper", "AssignNullToNotNullAttribute")]
     [SuppressMessage("ReSharper", "PossibleNullReferenceException")]
-    public class SortAndFindSameNumbersAtSameIndex
+    public class CountSameNumbers
     {
+        /// <summary>
+        /// The execution time is 0.13
+        /// </summary>
+        public static void RunMix()
+        {
+            var testCount = int.Parse(Console.ReadLine());
+            var tests = new string[testCount][];
+
+            for (var i = 0; i < testCount; i++)
+            {
+                tests[i] = new string[3];
+                tests[i][0] = Console.ReadLine().TrimEnd();
+                tests[i][1] = Console.ReadLine().TrimEnd();
+            }
+
+            foreach (var test in tests)
+            {
+                var left = StringScanner.GetPositiveInt(test[0], 10);
+                var right = StringScanner.GetPositiveInt(test[1], 10);
+                Console.WriteLine(left.Count(x => right.Any(y => x == y)));
+            }
+        }
+
         /// <summary>
         /// The execution time is 0.12
         /// </summary>
-        public static void Run()
+        public static void RunLoop()
         {
             var testCount = int.Parse(Console.ReadLine());
             var tests = new string[testCount][];
