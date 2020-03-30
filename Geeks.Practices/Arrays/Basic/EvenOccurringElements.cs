fe9de8f7471a99a27c73a00c6db8d77a0af7331d
Tayfun Yirdem <tayfunyirdem@gmail.com>
2020-03-30 14:22:24 +0300

    Arrays - Basic - EvenOccurringElements - Add - Solution - CompareToMix

--- Geeks.Practices/Arrays/Basic/EvenOccurringElements.cs
+++ Geeks.Practices/Arrays/Basic/EvenOccurringElements.cs
@@ -56,6 +56,30 @@ namespace Geeks.Practices.Arrays.Basic
     public class EvenOccurringElements
     {
         /// <summary>
+        /// The execution time is 0.09
+        /// </summary>
+        public static void RunCompareToMix()
+        {
+            var testCount = int.Parse(Console.ReadLine());
+            var tests = new string[testCount][];
+
+            for (var i = 0; i < testCount; i++)
+            {
+                tests[i] = new string[2];
+                tests[i][0] = Console.ReadLine();
+                tests[i][1] = Console.ReadLine().TrimEnd();
+            }
+
+
+            foreach (var test in tests)
+            {
+                var n = int.Parse(test[0]);
+                var numbers = StringScanner.GetPositiveInt(test[1], n);
+                Console.WriteLine(string.Join(' ', numbers.GroupBy(x => x).Where(x => (x.Count() & 1) == 0).Select(x => x.Key).OrderBy(x => x)));
+            }
+        }
+
+        /// <summary>
         /// The execution time is 0.13
         /// </summary>
         public static void RunMix()
