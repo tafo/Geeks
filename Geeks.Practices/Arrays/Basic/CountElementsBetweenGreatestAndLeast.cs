f6cf0f316c69d3962636791578fccbcdfe5a5311
Tayfun Yirdem <tayfunyirdem@gmail.com>
2020-03-30 13:07:40 +0300

    Arrays - Basic - CountElementsBetweenGreatestAndLeast - Add - Solution - Linq

--- Geeks.Practices/Arrays/Basic/CountElementsBetweenGreatestAndLeast.cs
+++ Geeks.Practices/Arrays/Basic/CountElementsBetweenGreatestAndLeast.cs
@@ -50,6 +50,29 @@ namespace Geeks.Practices.Arrays.Basic
     public class CountElementsBetweenGreatestAndLeast
     {
         /// <summary>
+        /// The execution time is 0.75
+        /// </summary>
+        public static void RunLinq()
+        {
+            var testCount = int.Parse(Console.ReadLine());
+            var tests = new string[testCount];
+
+            for (var i = 0; i < testCount; i++)
+            {
+                Console.ReadLine();
+                tests[i] = Console.ReadLine().TrimEnd();
+            }
+
+            foreach (var test in tests)
+            {
+                var numbers = test.Split(' ').Select(int.Parse).ToArray();
+                var min = numbers.Min();
+                var max = numbers.Max();
+                Console.WriteLine(numbers.Count(x => x > min && x < max));
+            }
+        }
+
+        /// <summary>
         /// The execution time is 0.38
         /// </summary>
         public static void RunCompareToMix()
