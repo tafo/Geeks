58995cbc77d11a99370f28ce13d5e09d6fffe031
Tayfun Yirdem <tayfunyirdem@gmail.com>
2020-03-30 13:04:46 +0300

    Arrays - Basic - CountElementsBetweenGreatestAndLeast - Add - Solution - CompareToMix

--- Geeks.Practices/Arrays/Basic/CountElementsBetweenGreatestAndLeast.cs
+++ Geeks.Practices/Arrays/Basic/CountElementsBetweenGreatestAndLeast.cs
@@ -52,6 +52,31 @@ namespace Geeks.Practices.Arrays.Basic
         /// <summary>
         /// The execution time is 0.38
         /// </summary>
+        public static void RunCompareToMix()
+        {
+            var testCount = int.Parse(Console.ReadLine());
+            var tests = new string[testCount][];
+
+            for (var i = 0; i < testCount; i++)
+            {
+                tests[i] = new string[2];
+                tests[i][0] = Console.ReadLine();
+                tests[i][1] = Console.ReadLine().TrimEnd();
+            }
+
+            foreach (var test in tests)
+            {
+                var n = int.Parse(test[0]);
+                var numbers = StringScanner.GetPositiveLong(test[1], n);
+                var min = numbers.Min();
+                var max = numbers.Max();
+                Console.WriteLine(numbers.Count(x => x > min && x < max));
+            }
+        }
+
+        /// <summary>
+        /// The execution time is 0.38
+        /// </summary>
         public static void RunMix()
         {
             var testCount = int.Parse(Console.ReadLine());
