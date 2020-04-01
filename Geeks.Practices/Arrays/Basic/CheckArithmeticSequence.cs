4f12ab5e05e5bf7e4b1a615d2b71a923f38ebae3
Tayfun Yirdem <tayfunyirdem@gmail.com>
2020-04-01 13:03:00 +0300

    Arrays - Basic - CheckArithmeticSequence - Add - Solution - Mix

--- Geeks.Practices/Arrays/Basic/CheckArithmeticSequence.cs
+++ Geeks.Practices/Arrays/Basic/CheckArithmeticSequence.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using Geeks.Practices.Helper;
 
 namespace Geeks.Practices.Arrays.Basic
@@ -44,6 +45,40 @@ namespace Geeks.Practices.Arrays.Basic
     [SuppressMessage("ReSharper", "PossibleNullReferenceException")]
     public class CheckArithmeticSequence
     {
+        /// <summary>
+        /// The execution time is 0.09
+        /// </summary>
+        public static void RunMix()
+        {
+            var testCount = int.Parse(Console.ReadLine());
+            var tests = new string[testCount][];
+
+            for (var i = 0; i < testCount; i++)
+            {
+                tests[i] = new string[2];
+                tests[i][0] = Console.ReadLine();
+                tests[i][1] = Console.ReadLine().TrimEnd();
+            }
+
+            foreach (var test in tests)
+            {
+                var n = int.Parse(test[0]);
+                var result = "YES";
+                if (n > 2)
+                {
+                    var numbers = StringScanner.GetPositiveInt(test[1], n);
+                    Array.Sort(numbers);
+                    var dif = numbers[1] - numbers[0];
+                    if (numbers.Skip(2).Select((x, i) => x - numbers[i + 1]).Any(x => x != dif))
+                    {
+                        result = "NO";
+                    }
+                }
+
+                Console.WriteLine(result);
+            }
+        }
+
         /// <summary>
         /// The execution time is 0.09
         /// </summary>
