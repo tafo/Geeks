9c486d681608cb3c4ab6466e49b8000d7c3e7fb1
Tayfun Yirdem <tayfunyirdem@gmail.com>
2020-04-01 11:14:00 +0300

    Arrays - Basic - JumpSum - Add - Solution - CompareToLinq

--- Geeks.Practices/Arrays/Basic/JumpSum.cs
+++ Geeks.Practices/Arrays/Basic/JumpSum.cs
@@ -64,6 +64,29 @@ namespace Geeks.Practices.Arrays.Basic
     public class JumpSum
     {
         /// <summary>
+        /// The execution time is 0.74
+        /// </summary>
+        public static void RunCompareToLinq()
+        {
+            var testCount = int.Parse(Console.ReadLine());
+            var tests = new string[testCount][];
+
+            for (var i = 0; i < testCount; i++)
+            {
+                tests[i] = new string[2];
+                tests[i][0] = Console.ReadLine();
+                tests[i][1] = Console.ReadLine().TrimEnd();
+            }
+
+            foreach (var test in tests)
+            {
+                // var n = int.Parse(test[0]); Skip the number of elements
+                var indexes = test[1].Split(' ').Select((x, i) => new {x = int.Parse(x), i}).OrderBy(x => x.x).Select(x => x.i).ToArray();
+                Console.WriteLine(indexes.Skip(1).Select((x, a) => Math.Abs(x - indexes[a])).Sum());
+            }
+        }
+
+        /// <summary>
         /// The execution time is 0.71
         /// </summary>
         public static void RunLinq()
