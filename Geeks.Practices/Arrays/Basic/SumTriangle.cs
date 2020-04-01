4c75a894f3250a76c70ef7da95bda177829d4bf8
Tayfun Yirdem <tayfunyirdem@gmail.com>
2020-04-01 14:52:00 +0300

    Arrays - Basic - SumTriangle - Add - Solution - Linq

--- Geeks.Practices/Arrays/Basic/SumTriangle.cs
+++ Geeks.Practices/Arrays/Basic/SumTriangle.cs
@@ -57,6 +57,34 @@ namespace Geeks.Practices.Arrays.Basic
         /// <summary>
         /// The execution time is 0.14
         /// </summary>
+        public static void RunLinq()
+        {
+            var testCount = int.Parse(Console.ReadLine());
+            var tests = new string[testCount][];
+
+            for (var i = 0; i < testCount; i++)
+            {
+                tests[i] = new string[2];
+                tests[i][0] = Console.ReadLine();
+                tests[i][1] = Console.ReadLine().TrimEnd();
+            }
+
+            foreach (var test in tests)
+            {
+                var n = int.Parse(test[0]);
+                var numbers = test[1].Split(' ').Select(int.Parse).ToArray();
+                var result = numbers.Concat(null);
+                while (n-- > 0)
+                {
+                    result = (numbers = numbers.Skip(1).Select((x, i) => x + numbers[i]).ToArray()).Concat(result);
+                }
+                Console.WriteLine(string.Join(' ', result));
+            }
+        }
+
+        /// <summary>
+        /// The execution time is 0.14
+        /// </summary>
         public static void RunMix()
         {
             var testCount = int.Parse(Console.ReadLine());
