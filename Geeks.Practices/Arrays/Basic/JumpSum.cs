9c4e062d6d8559626c543126e86eb6feba274157
Tayfun Yirdem <tayfunyirdem@gmail.com>
2020-04-01 11:10:12 +0300

    Arrays - Basic - JumpSum - Add - Solution - Mix

--- Geeks.Practices/Arrays/Basic/JumpSum.cs
+++ Geeks.Practices/Arrays/Basic/JumpSum.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using Geeks.Practices.Helper;
 
 namespace Geeks.Practices.Arrays.Basic
@@ -62,6 +63,29 @@ namespace Geeks.Practices.Arrays.Basic
     [SuppressMessage("ReSharper", "PossibleNullReferenceException")]
     public class JumpSum
     {
+        /// <summary>
+        /// The execution time is 0.49
+        /// </summary>
+        public static void RunMix()
+        {
+            var testCount = int.Parse(Console.ReadLine());
+            var tests = new string[testCount][];
+
+            for (var i = 0; i < testCount; i++)
+            {
+                tests[i] = new string[2];
+                tests[i][0] = Console.ReadLine();
+                tests[i][1] = Console.ReadLine().TrimEnd();
+            }
+
+            foreach (var test in tests)
+            {
+                var n = int.Parse(test[0]);
+                var numbers = StringScanner.GetPositiveInt(test[1], n).Select((x,i) => new{Number = x, Index = i}).OrderBy(x => x.Number).ToArray();
+                Console.WriteLine(numbers.Skip(1).Select((x, i) => Math.Abs(x.Index - numbers[i].Index)).Sum());
+            }
+        }
+
         /// <summary>
         /// The execution time is 0.45
         /// </summary>
