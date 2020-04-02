be244e517d086edca1f5c1dffedd5f39d102ec35
Tayfun Yirdem <tayfunyirdem@gmail.com>
2020-04-02 18:34:47 +0300

    Arrays - Basic - CountDistinctAbsoluteValues - Add - Solution - Mix

--- Geeks.Practices/Arrays/Basic/CountDistinctAbsoluteValues.cs
+++ Geeks.Practices/Arrays/Basic/CountDistinctAbsoluteValues.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using Geeks.Practices.Helper;
 
 namespace Geeks.Practices.Arrays.Basic
@@ -41,6 +42,20 @@ namespace Geeks.Practices.Arrays.Basic
     [SuppressMessage("ReSharper", "PossibleNullReferenceException")]
     public class CountDistinctAbsoluteValues
     {
+        /// <summary>
+        /// The execution time is 0.08
+        /// </summary>
+        public static void RunMix()
+        {
+            var testCount = int.Parse(Console.ReadLine());
+
+            for (var i = 0; i < testCount; i++)
+            {
+                var n = int.Parse(Console.ReadLine());
+                Console.WriteLine(StringScanner.GetAbsoluteInt(Console.ReadLine().TrimEnd(), n).Distinct().Count());
+            }
+        }
+
         /// <summary>
         /// The execution time is 0.09
         /// </summary>
